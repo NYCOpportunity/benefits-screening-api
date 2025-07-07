a3a67664fdb1534543cd5207d79041bcefd467a0
Koji Wong <kojirwong@gmail.com>
2025-07-06 17:32:13 -0700

    oops lol minor fix on unit test

--- tests/data/sample_eligibility_rule.py
+++ tests/data/sample_eligibility_rule.py
@@ -4,8 +4,12 @@ import json
 
 def sample_eligibility_rule() -> EligibilityRequest:
 
-    userInfo = 'tests/data/eligibility-program-test-payload.json' 
-    data = json.load(userInfo)
-    eligibility_request = validate_request(data)
+    file_path = 'tests/data/eligibility-program-test-payload.json'
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    is_valid, eligibility_request, error_messages = validate_request(data)
+    if not is_valid:
+        raise ValueError(f"Sample data validation failed: {error_messages}")
     return eligibility_request
 
+    
